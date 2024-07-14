83eab02b8cb45b8132f2390885826f955bfb7784
Jason Nguyen <128450487+tennydev@users.noreply.github.com>
2024-07-14 21:45:59 +0700

    Minor fix to match endpoint mapping

--- src/main/java/com/rvlt/ecommerce/controller/InventoryController.java
+++ src/main/java/com/rvlt/ecommerce/controller/InventoryController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/inventory")
+@RequestMapping("/inventories")
 public class InventoryController {
     @Autowired
     private InventoryService inventoryService;
