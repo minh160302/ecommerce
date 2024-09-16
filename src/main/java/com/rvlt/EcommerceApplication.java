562cbe79cb8fbd4107ad78929ac02da6e922ae03
Minh Nguyen <minh160302@gmail.com>
2024-09-16 16:17:25 -0700

    config Render deployment attempt #2

--- src/main/java/com/rvlt/EcommerceApplication.java
+++ src/main/java/com/rvlt/EcommerceApplication.java
@@ -28,7 +28,7 @@ public class EcommerceApplication {
       // CORS
       @Override
       public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:3000", "https://shop-frend.vercel.app/");
+        registry.addMapping("/**").allowedOrigins("http://localhost:3000", "https://blueprint-ecommerce.vercel.app/");
       }
 
       // Path prefix
